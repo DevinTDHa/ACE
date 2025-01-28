89967caf719c71bae416eadfb421157612a82505
Devin Ha <t.ha@tu-berlin.de>
2025-01-28 15:02:02 +0100

    RetinaMNIST running

--- scripts/regression/run_retinaMNIST.sh
+++ scripts/regression/run_retinaMNIST.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=gpu-2h
 #SBATCH --gpus-per-node=1
 #SBATCH --ntasks-per-node=4
-#SBATCH --constraint=80gb
+#SBATCH --constraint="80gb|40gb"
 #SBATCH --output=logs/job-%x-%j.out
 #SBATCH --chdir=/home/tha/ACE/
 
@@ -38,15 +38,17 @@
 # sampling_dilation=15,  # Dilation size for the mask generation
 # # QUERY AND TARGET LABEL
 # # DATASET
-MODEL_FLAGS="--attention_resolutions 32,16,8 --class_cond False --diffusion_steps 500 --learn_sigma True --noise_schedule linear --num_channels 128 --num_head_channels 64 --num_res_blocks 2 --resblock_updown True --use_fp16 True --use_scale_shift_norm True"
-MODEL_PATH="/home/tha/ACE/pretrained/celebahq-ddpm.pt"
-CONFIDENCE_THRESHOLD="0.05"
-IMAGE_FOLDER="/data/CelebAMask-HQ"
-IMAGE_SIZE="256"
+# MODEL_FLAGS="--attention_resolutions 16,8 --class_cond False --diffusion_steps 1000 --learn_sigma False --noise_schedule linear --num_channels 128 --num_heads 4 --num_res_blocks 2 --resblock_updown True --use_fp16 True --use_scale_shift_norm True"
+MODEL_FLAGS="" # keep all defaults?
+MODEL_PATH="/home/tha/runs/ace/retinaMNIST_ddpm/ema_0.9999_870000.pt"
+# MODEL_PATH="/home/tha/runs/ace/retinaMNIST_ddpm/model870000.pt"
+RMODEL_PATH="/home/tha/thesis_runs/regressor/retinaMNIST-128/version_0/checkpoints/last.ckpt"
+RORACLE_PATH="/home/tha/thesis_runs/regressor/retinaMNIST_oracle-128/version_0/checkpoints/last.ckpt"
+IMAGE_SIZE="128"
 
 # Attack parameters
-if [ "$#" -gt 5 ]; then
-    echo "Usage: $0 <attack_method=PGD> <attack_step=1.0> <dist_l1=0.0> <dist_l2=0.0> <rmodel_path>"
+if [ "$#" -gt 4 ]; then
+    echo "Usage: $0 <attack_method=PGD> <attack_step=1.0> <dist_l1=0.0> <dist_l2=0.0>"
     exit 1
 fi
 
@@ -54,44 +56,33 @@ ATTACK_METHOD=${1:-PGD}
 ATACK_STEP=${2:-1.0}
 DIST_L1=${3:-0.0} # Dist does not work well, no real results if enabled
 DIST_L2=${4:-0.0}
-RMODEL_PATH=${5:-"/home/tha/thesis_runs/regressor/imdb_wiki_densenet_linear_only-256/version_0/checkpoints/last.ckpt"}
-RORACLE_PATH="/home/tha/thesis_runs/regressor/imdb_wiki_densenet_fullft-256/version_0/checkpoints/last.ckpt"
-if [[ "$RMODEL_PATH" == *"linear_only"* ]]; then
-    LINEAR_ONLY=1
-else
-    LINEAR_ONLY=0
-fi
+CONFIDENCE_THRESHOLD="0.05"
 
-NUM_SAMPLES=3000
-MAX_STEPS=200
-BATCH_SIZE=8
-
-NAME="CelebaHQ_FR-method=${ATTACK_METHOD}-step=${ATACK_STEP}-dist_l1=${DIST_L1}-dist_l2=${DIST_L2}-rmodel_linear=${LINEAR_ONLY}"
-OUTPUT_PATH="/home/tha/thesis_runs/ace/celebahq/$NAME"
+NUM_SAMPLES=20
+BATCH_SIZE=1
+NAME="retinaMNIST-method=${ATTACK_METHOD}_step=${ATACK_STEP}_dist_l1=${DIST_L1}_dist_l2=${DIST_L2}"
+OUTPUT_PATH="/home/tha/thesis_runs/ace/retinaMNIST/$NAME"
 
 echo "Runnning $NAME"
 # Run the Python script with the arguments
 apptainer run \
-    -B /home/space/datasets:/home/space/datasets \
-    -B /home/space/datasets-sqfs/CelebAMask-HQ.sqfs:/data/CelebAMask-HQ:image-src=/ \
     --nv \
     ~/apptainers/thesis.sif \
-    python main_regression_celebahq.py $MODEL_FLAGS \
+    python main_regression_retinaMNIST.py $MODEL_FLAGS \
     --model_path=$MODEL_PATH \
     --rmodel_path=$RMODEL_PATH \
     --roracle_path=$RORACLE_PATH \
-    --batch_size=$BATCH_SIZE \
     --attack_step=$ATACK_STEP \
     --confidence_threshold=$CONFIDENCE_THRESHOLD \
-    --image_folder=$IMAGE_FOLDER \
     --image_size=$IMAGE_SIZE \
     --output_path=$OUTPUT_PATH \
     --num_samples=$NUM_SAMPLES \
+    --batch_size=$BATCH_SIZE \
     --exp_name=$NAME \
     --attack_method=$ATTACK_METHOD \
-    --attack_iterations=$MAX_STEPS \
+    --attack_iterations=50 \
     --attack_joint=True \
     --dist_l1=$DIST_L1 \
     --dist_l2=$DIST_L2 \
-    --timestep_respacing=25 \
-    --sampling_time_fraction=0.2
+    --timestep_respacing=50 \
+    --sampling_time_fraction=0.1
